400271c5f218da22266b85a73b8dadd8ed9a13f6
zvekovius <zvekovius@veknet.net>
2020-06-16 19:44:59 -0700

    Incremental progress

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ### Purpose ###
 # Get the box ready to run ansible. This is meant to be slim.
-# It shouldn't do anything other than getting ready for Ansible.
+# It shall not do anything other than getting ready for Ansible.
+# It shall be idempontent.
 ### End Purpose ###
 
 sudo apt-get update
@@ -16,7 +17,10 @@ python3 -m venv /tmp/provision-venv
 source /tmp/provision-venv/bin/activate
 
 # Install ansible for usage
-pip install ansible==2.8
+# We are okay with pulling the latest 2.9 because we will be running this pipeline
+# On a weekly basis to make sure it still works with 2.9.
+# It'll be an explicit decision to upgrade to a new major version
+pip install ansible==2.9.*
 
 # Everything else at this point is ansible.
 ansible-playbook ansible/playbooks/provision.yml
